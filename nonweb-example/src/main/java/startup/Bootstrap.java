5eaf372ba2d9169769aad2dc6f2f04184edfb628
liqiulin <liqiulin@fxiaoke.com>
2016-12-13 20:31:49 +0800

    nonweb-example

---
+++ nonweb-example/src/main/java/startup/Bootstrap.java
@@ -0,0 +1,14 @@
+package startup;
+
+/**
+ * Created by liqiulin on 2016/12/13.
+ */
+public class Bootstrap {
+    public static void main(String[] args) throws Exception {
+//        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:spring-config.xml");
+//        ctx.registerShutdownHook();
+
+        System.out.println("Bootstrap start success...");
+        Thread.currentThread().join();
+    }
+}
